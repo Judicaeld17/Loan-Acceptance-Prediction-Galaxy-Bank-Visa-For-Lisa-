66a38dcd66b4069761ebae553e6924dd7a3a7ce7
Judicael Mahoutin DJIDONOU <117901002+Judicaeld17@users.noreply.github.com>
2024-06-17 15:43:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
-
 import streamlit as st
 import joblib
 import numpy as np
 
 #load the model
-model=joblib.load(model.pkl)
+model=joblib.load('model.pkl')
 
 #Titleof the app
 st.title("Loan offer acceptance predictor")
 
-#Input fields
-input_data= st.text_input("Enter input data : CSV ")
-
 #Prediction Button
+
+# ID = st.number_input("ID", value=2)
+Age = st.number_input("Age", value=45)
+Experience = st.number_input("Experience (years)", value=19)
+Income = st.number_input("Income ($)", value=34000)
+ZIP_Code = st.number_input("ZIP Code", value=90089)
+Family = st.number_input("Family (members)", value=3)
+CCAvg = st.number_input("Average Credit Card Spending ($)", value=1500)
+Education = st.selectbox("Education Level", options=[1, 2, 3], index=0)
+Mortgage = st.number_input("Mortgage ($)", value=0)
+Securities_Account = st.selectbox("Securities Account", options=["Yes", "No"], index=0)
+CD_Account = st.selectbox("CD Account", options=["Yes", "No"], index=1)
+Online = st.selectbox("Online", options=["Yes", "No"], index=1)
+CreditCard = st.selectbox("Credit Card", options=["Yes", "No"], index=1)
+
+# Button to make predictions
 if st.button("Predict"):
-    input_array=np.array([float(i) for i in input_data.split(',')]).reshape(1,-1)
+    # Convert input data to numpy array
+    input_array = np.array([Age, Experience, Income, ZIP_Code, Family, CCAvg, 
+                            Education, Mortgage, 
+                            1 if Securities_Account == "Yes" else 0,
+                            1 if CD_Account == "Yes" else 0,
+                            1 if Online == "Yes" else 0,
+                            1 if CreditCard == "Yes" else 0]).reshape(1, -1)
     
     #Make prediction
     prediction=model.predict(input_array)
@@ -22,5 +40,5 @@ if st.button("Predict"):
     #Display prediction
     st.write(f"Prediction: {prediction[0]}")
 
-
+    
     
